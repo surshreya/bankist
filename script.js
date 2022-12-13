b6b90994c6e3c234f1569f0dbba913201bd7c77e
surshreya <surshreya.0727@gmail.com>
2022-12-13 20:22:55 +0530

    initial js setup data

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 "use strict";
+
+// Data
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Selecting Elements
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+const labelWelcome = document.querySelector(".welcome");
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const btnLogin = document.querySelector(".login__btn");
+
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const btnSort = document.querySelector(".btn--sort");
+
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputTransferTo = document.querySelector(".form__input--to");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const btnLoan = document.querySelector(".form__btn--loan");
+
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+const btnClose = document.querySelector(".form__btn--close");
+
+const labelTimer = document.querySelector(".timer");
